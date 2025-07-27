730ce66ebc896bb2e216a48f11fe7959b1abebdf
Mills888dev <104231106+Mills888dev@users.noreply.github.com>
2025-07-27 02:27:30 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // 🔗 Your published Google Sheet (as CSV)
-const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3eZlY581bQHv8_mK9eCmPwwJgrbTTXC9a1K7o5h_yN6jfWgI6ul_pWH-XPlItITXj1V1IXdJJL0k0/pub?gid=0&single=true&output=csv";
+const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3eZlY581bQHv8_mK9eCmPwwJgrbTTXC9a1K7o5h_yN6jfWgI6ul_pWH-XPlItITXj1V1IXdJJL0k0/pub?output=csv";
 
 function fetchSheetData(callback) {
   fetch(sheetURL)
