4af662f81af7b6686d40e66470dcb8a387f7336c
Mills888dev <104231106+Mills888dev@users.noreply.github.com>
2025-07-28 13:35:17 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -95,9 +95,9 @@ function renderGraph(data) {
     userZoomingEnabled: false,
     boxSelectionEnabled: false
   });
-}
 
-});
+
+}
 function refreshGraph() {
   const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3eZlY581bQHv8_mK9eCmPwwJgrbTTXC9a1K7o5h_yN6jfWgI6ul_pWH-XPlItITXj1V1IXdJJL0k0/pub?output=csv";
   document.getElementById('cy').innerHTML = '';
